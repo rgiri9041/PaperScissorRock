bf8954994dd0a8ba11ee075e61ed97c488928cc9
Raj Kumar Giri <rgiri9041@gmail.com>
2024-07-28 17:13:05 +0545

    Final game. Now its ready to play.

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
-const a = "Hello World"
-const b = " I am Raj"
-let final = a + b;
-console.log(final);
+function getComputerChoice() {
+    const choice = ["rock", "scissor", "paper"]
+    const randomIndex = Math.floor(Math.random() * choice.length);
+    return choice[randomIndex];
+}
+
+function getHumanChoice() {
+    let choice = prompt("Enter your choice").toLowerCase();
+    while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissor') {
+        choice = prompt("Enter your choice that should be 'rock', 'scissor' or 'paper").toLowerCase();
+    }
+    return choice;
+}
+function playRound(humanChoice, computerChoice) {
+    console.log(`computer choice is ${computerChoice}, Your choice is ${humanChoice}`)
+    if (humanChoice === computerChoice) {
+
+        return "draw"
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissor" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissor" && computerChoice === "paper") {
+        return "win"
+
+    }
+    else {
+        ;
+        return "loose"
+    }
+
+}
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+    const round = 5;
+
+    for (let i = 0; i < round; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        let result = playRound(humanSelection, computerSelection);
+        if (result === "win") {
+            console.log("Your win this round")
+            humanScore++
+        }
+        else if (result === "loose") {
+            console.log("You loose this round")
+            computerScore++
+        }
+        else {
+            console.log("Its a draw in this round")
+        }
+
+
+    }
+    if (humanScore > computerScore) {
+        console.log("Congratulation you win the game.")
+    }
+    else if (humanScore < computerScore) {
+        console.log("Sorry! You loose the game. Try again next time.")
+
+    }
+    else {
+        console.log("Draw. Try again")
+    }
+}
+playGame()
